dd86a1bef1f08f6ebff088fac6e8f012d60d4940
Lokeshwaranramu <lokeshramu16@gmail.com>
2017-06-08 20:48:09 +0530

    Add files via upload

---
+++ netty/src/main/java/org/lokesh/netty/ClientHandler.java
@@ -0,0 +1,19 @@
+package org.lokesh.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+ 
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+ 
+  @Override
+  public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    LoopBackTimeStamp ts = (LoopBackTimeStamp) msg;
+    ctx.writeAndFlush(ts);
+  }
+ 
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    cause.printStackTrace();
+    ctx.close();
+  }
+}
